f40f41fb6455f1571190ea9996ea3c2b7c9471ff
Antonino Ragusa <antoragu@yahoo.it>
2026-01-06 13:02:40 +0100

    correzione

---
+++ js/firebase-config.js
@@ -0,0 +1,11 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyA-Vm3aVHy7m230s_YGuBYgL0MRlnF2hts",
+    authDomain: "scheda-personaggio-cod.firebaseapp.com",
+    projectId: "scheda-personaggio-cod",
+    storageBucket: "scheda-personaggio-cod.firebasestorage.app",
+    messagingSenderId: "594233148940",
+    appId: "1:594233148940:web:9c6321b433735084269029"
+};
+// Inizializza Firebase
+firebase.initializeApp(firebaseConfig);
+var db = firebase.firestore();
